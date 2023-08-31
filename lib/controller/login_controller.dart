640e35e8ab04e21d24554d1e9ce1e78f44cb859f
seobss82@gmail.com <seobss82@gmail.com>
2023-08-31 23:12:50 +0900

    [chg] : 로그인 관련 commit 누락 파일 추가.

--- lib/controller/login_controller.dart
+++ lib/controller/login_controller.dart
@@ -7,10 +7,17 @@ class LoginController extends GetxController {
   var isLoggedIn = false.obs; // Reactive 상태
   final Preference _preference = getIt.get<Preference>();
 
+  @override
+  void onInit() {
+    super.onInit();
+    isLoggedIn.value = _preference.read(PreferenceType.isLogin) ?? false;
+  }
+
   Future<bool> login(String password) async {
     if (password == '12345') {
       isLoggedIn.value = true;
       _preference.write(PreferenceType.isLogin, true);
+      _preference.isLogin = true;
       return true;
     }
     return false;
